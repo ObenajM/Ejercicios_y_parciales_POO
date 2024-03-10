656f399c672d6ffb0f790309727f03321202975b
Uribe-37 <juribeca@unal.edu.co>
2024-03-10 18:39:40 -0500

    Check

--- Primer_parcial/Punto_23.py
+++ Primer_parcial/Punto_23.py
@@ -1,7 +1,7 @@
 import math
-a = float(input("Ingrese el coeficiente 'a': "))
-b = float(input("Ingrese el coeficiente 'b': "))
-c = float(input("Ingrese el coeficiente 'c': "))
+a = float(input("Ingrese el coeficiente A: "))
+b = float(input("Ingrese el coeficiente B: "))
+c = float(input("Ingrese el coeficiente C: "))
 
 discriminante = math.pow(b,2) - 4*a*c
 
@@ -10,7 +10,10 @@ if discriminante > 0:
     raiz2 = (-b - math.sqrt(discriminante)) / (2*a)
     print("X_1 = ",raiz1," X_2 = ",raiz2) 
 elif discriminante == 0:
-    raiz = -b / (2*a)
+    if b == 0:
+        raiz = 0
+    else:
+        raiz = -b / (2*a)
     print("La única solución es: ",raiz) 
 else:
     print("No hay soluciones reales")
