cbecc50d8101721cbea0f25f0be0bbd9a31bf4b2
Obeimar Benavides <obenavides@unal.edu.co>
2024-03-11 12:32:03 -0500

    Corrección método punto 20

--- Actividad 2/Punto_20.py
+++ Actividad 2/Punto_20.py
@@ -7,11 +7,15 @@ class recta(object):
     def parametros(self):
         m = (self.y2 - self.y1) / (self.x2 - self.x1)
         b = self.y1 - m * self.x1
-        A,B,C=m*(m%1)**(-1),-(m%1)**(-1),(-m*self.x1+self.y1)*(m%1)**(-1);
-        return (A,B,C)
+        if m%1 != 0:
+            A,B,C=m*(m%1)**(-1),-(m%1)**(-1),(-m*self.x1+self.y1)*(m%1)**(-1);
+            return (A,B,C)
+        else:
+            A,B,C=m,-1,-m*self.x1+self.y1;
+            return (A,B,C)
     
     def __str__(self):
         return "La escuación de la recta es {}x + {}y + {} = 0".format(self.parametros()[0],self.parametros()[1],self.parametros()[2])
 
-recta1 = recta(-1,2,0,3.5)
+recta1 = recta(-2,5,1,2)
 print(recta1.parametros())
