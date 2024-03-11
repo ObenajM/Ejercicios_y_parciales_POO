684232039d8921803dbbea2a7bf56050a48de1f5
Uribe-37 <juribeca@unal.edu.co>
2024-03-10 22:09:01 -0500

    Check

--- Actividad 2/Punto_22.py
+++ Actividad 2/Punto_22.py
@@ -1,10 +1,23 @@
+class Empleado:
+    def __init__(self, nombre, salario_por_hora, horas_trabajadas):
+        self.nombre = nombre
+        self.salario_por_hora = salario_por_hora
+        self.horas_trabajadas = horas_trabajadas
+
+    def calcular_salario_mensual(self):
+        return self.salario_por_hora * self.horas_trabajadas
+
+    def imprimir_salario(self):
+        salario_mensual = self.calcular_salario_mensual()
+        if salario_mensual > 450000:
+            print(self.nombre, ":", salario_mensual)
+        else:
+            print(self.nombre)
+
+
 nombre_empleado = input("Ingrese el nombre del empleado: ")
 salario_por_hora = float(input("Ingrese el salario por hora del empleado: "))
 horas_trabajadas = float(input("Ingrese el número de horas trabajadas en el mes: "))
 
-salario_mensual = salario_por_hora * horas_trabajadas
-if salario_mensual > 450000:
-    print(nombre_empleado, ":", salario_mensual)
-else:
-    print(nombre_empleado)
-
+empleado = Empleado(nombre_empleado, salario_por_hora, horas_trabajadas)
+empleado.imprimir_salario()
