c3bfca386494d3062d62f2a6cdcc63b83fd1f7ba
Obeimar Benavides <obenavides@unal.edu.co>
2024-08-18 18:41:14 -0500

    gitignore

---
+++ Actividad 4/Ejercicio 4.1 Herencia/Cuenta.py
@@ -0,0 +1,34 @@
+class Cuenta:
+    def __init__(self, saldo, tasa_anual):
+        self._saldo = saldo
+        self._num_consignaciones = 0
+        self._num_retiros = 0
+        self._tasa_anual = tasa_anual
+        self._comision_mensual = 0.0
+
+    def consignar(self, cantidad):
+        self._saldo += cantidad
+        self._num_consignaciones += 1
+
+    def retirar(self, cantidad):
+        if cantidad <= self._saldo:
+            self._saldo -= cantidad
+            self._num_retiros += 1
+        else:
+            print("Saldo insuficiente")
+
+    def calcular_interes_mensual(self):
+        interes_mensual = (self._tasa_anual / 12) / 100 * self._saldo
+        self._saldo += interes_mensual
+
+    def extracto_mensual(self):
+        self._saldo -= self._comision_mensual
+        self.calcular_interes_mensual()
+        self._comision_mensual = 0.0  # Reiniciar la comisión
+
+    def imprimir(self):
+        print(f"Saldo: {self._saldo}")
+        print(f"Número de consignaciones: {self._num_consignaciones}")
+        print(f"Número de retiros: {self._num_retiros}")
+        print(f"Tasa anual: {self._tasa_anual}")
+        print(f"Comisión mensual: {self._comision_mensual}")
