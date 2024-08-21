5eb977afbdc9036227c43c44d85f514503ebf5fc
Uribe-37 <juribeca@unal.edu.co>
2024-08-20 19:57:05 -0500

    Check

--- Actividad 4/Ejercicio 4.1 Herencia/main.py
+++ Actividad 4/Ejercicio 4.1 Herencia/main.py
@@ -6,5 +6,5 @@ cuenta_ahorros = CuentaAhorros(100000, 10)
 cuenta_ahorros.consignar(50000)
 cuenta_ahorros.retirar(70000)
 cuenta_ahorros.extracto_mensual()
-cuenta_ahorros.imprimir()
+cuenta_ahorros.imprimir() 
 
