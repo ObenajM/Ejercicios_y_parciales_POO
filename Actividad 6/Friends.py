9fec6dc14c2cb29bed976f50f2ae59bb79a88cbe
Obeimar Benavides <obenavides@unal.edu.co>
2024-09-22 20:20:52 -0500

    Actividad 6

---
+++ Actividad 6/Friends.py
@@ -0,0 +1,13 @@
+from Main import Main
+import tkinter as tk
+
+class friend:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        self.mainform= Main(tk.Tk())
+        
+if __name__=="__main__":
+    Main=friend().main()
+    
